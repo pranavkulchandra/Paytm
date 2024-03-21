2c0893bd20d1b2f1e60cd6e1b027c2f65a8b223e
pranavkulchandra <109320936+pranavkulchandra@users.noreply.github.com>
2024-03-21 14:59:45 -0700

    updated trasaction API

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -26,6 +26,51 @@ router.get("/balance",authMiddleware, async( req, res)=> {
     }
 })
 
+router.post("/transfer", authMiddleware, async (req, res) => { 
+
+    const session = await mongoose.startSession(); 
+
+     session.startTransaction()
+try {
+    
+
+    const { amount , toUserId } = req.body;
+
+
+    const fromUserAccount = await Account.findOne({userId : req.userId})
+    if (!fromUserAccount) { 
+        session.abortTransaction();
+        return res.status(401).json({ Message : "User not found"})
+    }
+
+    if(fromUserAccount.balance < amount) { 
+         session.abortTransaction(); 
+       return  res.status(403).json({ Message : "Insufficient Balance "})
+    }
+
+    const toUserAccount = await Account.findOne({ userId : toUserId})
+    console.log(toUserAccount)
+    
+
+
+    if(!toUserAccount) { 
+        session.abortTransaction()
+        return res.status(401).json({Message : "To user not found."})
+    }
+
+    await Account.updateOne({ userId : req.userId}, {$inc : { balance : -amount}}, {session});
+    await Account.updateOne({ userId : toUserId}, { $inc : { balance : amount}}, {session});
+
+    session.commitTransaction();
+    res.status(200).json({ MEssage  : "Transaction Successfull"})
+    
+
+} catch (error) {
+   return res.status(400).json({Error : error, Message : "From Catch "})
+}
+
+})
+
 
 
 module.exports = router
